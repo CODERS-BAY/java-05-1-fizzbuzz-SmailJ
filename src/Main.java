0d9a80a6b6ef8b483ad105a725a7d3aecc99384e
Smail <smail.jusic16@gmail.com>
2020-08-31 10:33:11 +0200

    Logik gecheckt

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++){
-            if(i % 3 == 0){
-                System.out.println("Buzz "+i);
-            } else {
-                System.out.println("Fizz "+i);
+        for (int i = 1; i < 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println("Fizz " + i);
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz " + i);
+            } else{
+                System.out.println(i);
             }
         }
 
